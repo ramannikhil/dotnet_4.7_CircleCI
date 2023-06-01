43a58fc1d256f2a07aa5487306b7a539033bd399
Raman Nikhil <111501090+ramannikhil@users.noreply.github.com>
2023-06-01 14:19:02 +0530

    added new files

---
+++ CircleCITestPackage_Dotnet_4.7/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CircleCITestPackage_Dotnet_4._7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<int> list = new List<int>();
+
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("done");
+        }
+    }
+}
