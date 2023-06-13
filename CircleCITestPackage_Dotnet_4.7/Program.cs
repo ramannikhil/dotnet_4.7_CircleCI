68dea199b2b2042dc781f2fde6a8d3545e13222e
Raman Nikhil <111501090+ramannikhil@users.noreply.github.com>
2023-06-13 14:17:49 +0530

    added new uninstall software

--- CircleCITestPackage_Dotnet_4.7/Program.cs
+++ CircleCITestPackage_Dotnet_4.7/Program.cs
@@ -6,19 +6,19 @@ using System.Threading.Tasks;
 
 namespace CircleCITestPackage_Dotnet_4._7
 {
-    internal class Program
+  internal class Program
+  {
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
 
-            List<int> list = new List<int>();
+      // List<int> list = new List<int>();
 
-            foreach (var item in list)
-            {
-                Console.WriteLine(item);
-            }
+      // foreach (var item in list)
+      // {
+      //     Console.WriteLine(item);
+      // }
 
-            Console.WriteLine("done");
-        }
+      Console.WriteLine("hello world dotnet framework");
     }
+  }
 }
